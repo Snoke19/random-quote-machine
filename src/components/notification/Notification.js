1e1d4feec51a8728c17c63c78d1bedc9dbe8f11e
Snoke19 <alex.developer27@outlook.com>
2024-09-21 00:34:58 +0300

    refactored notification

--- src/components/notification/Notification.js
+++ src/components/notification/Notification.js
@@ -1,29 +1,14 @@
 import React from "react";
+import './Notification.css';
 
 export default function Notification({ notificationInfo }) {
 
     const { show, info } = notificationInfo;
 
-    const styles = {
-        toast: {
-            position: 'fixed',
-            bottom: '20px',
-            left: '50%',
-            transform: 'translateX(-50%)',
-            backgroundColor: '#333',
-            color: '#fff',
-            padding: '10px 20px',
-            borderRadius: '5px',
-            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
-            zIndex: 1000,
-            opacity: 0.9
-        },
-    };
-
     return (
         <>
-            {(show) && (
-                <div style={styles.toast}>
+            {show && (
+                <div className="toast">
                     <p>{info}</p>
                 </div>
             )}
