3f64b7d7f7545ea61b69f79bd42a45c3ba7078d0
Snoke19 <alex.developer27@outlook.com>
2024-09-23 22:27:56 +0300

    renamed Footer folder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import QuoteBox from "./components/QuoteBox/QuoteBox";
-import Footer from "./components/footer/Footer";
+import Footer from "./components/Footer/Footer";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
