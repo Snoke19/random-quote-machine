bdb98872a4e6560f188ce5b68ffc05fc24d7ac36
Alex <alex.developer27@outlook.com>
2026-06-14 00:45:49 +0300

    added a simple test, add env file and refactored for using env vars

--- src/utils/AxiosClient.js
+++ src/utils/AxiosClient.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
-  timeout: 6000,
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
+    baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080",
+    timeout: parseInt(process.env.REACT_APP_API_TIMEOUT) || 6000,
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
 });
 
 export default AxiosInstance;
